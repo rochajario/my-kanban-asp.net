142f1094bd04add156d14c3cb4588540df4c5aed
Jario Rocha <rochajario@gmail.com>
2023-09-02 13:23:08 -0300

    chore: Removing experimental code

--- my-kanban-mvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace my_kanban_mvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
